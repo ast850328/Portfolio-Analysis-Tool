eea62fdd5df5e746d62ff462f3dd936cea271b95
An-Shiang Tsai <ast850328@gmail.com>
2020-02-12 00:30:42 +0800

    Add: Selector class

---
+++ Selector.py
@@ -0,0 +1,11 @@
+class Selector:
+    def __init__(self, number, ranking):
+        self.number = number
+        self.ranking = ranking
+
+    def select(self, df):
+        pass
+
+    def cal_performance(self, df):
+        # Equity, MDD, Equity/MDD, [MAR, CAGR]
+        pass
