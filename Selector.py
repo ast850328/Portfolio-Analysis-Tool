e135554c966dad42d019abcc9ca63c4890b8640b
An-Shiang Tsai <ast850328@gmail.com>
2020-02-15 22:29:42 +0800

    Add: Selector class

--- Selector.py
+++ Selector.py
@@ -1,11 +1,45 @@
+import pandas as pd
 class Selector:
-    def __init__(self, number, ranking):
+    def __init__(self, number, target):
         self.number = number
-        self.ranking = ranking
+        self.target = target
+        if target == 'MDD':
+            self.biggest = False
+        else:
+            self.biggest = True
 
     def select(self, df):
-        pass
+        df_result = self._cal_performance(df)
+        if self.biggest:
+            df_selected = df_result.nlargest(self.number, self.target)
+        else:
+            df_selected = df_result.nsmallest(self.number, self.target)
+        selected_columns = df_selected.index.tolist()
+        return selected_columns
 
-    def cal_performance(self, df):
+    def _cal_performance(self, df):
         # Equity, MDD, Equity/MDD, [MAR, CAGR]
-        pass
+        columns = df.columns.tolist()
+        columns.remove('Date')
+        data = {}
+
+        for col in columns:
+            df_col = df.loc[:, [col]]
+            buy_price = df_col.iloc[0].values[0]
+            sell_price = df_col.iloc[-1].values[0]
+            equity = sell_price - buy_price
+            MDD = 0
+            peak = buy_price
+            for index, row in df_col.iterrows():
+                price = row[col]
+                if price >= peak:
+                    peak = price
+                DD = price - peak
+                if DD <= MDD:
+                    MDD = DD
+            MDD = 0 - MDD
+            equity_to_MDD = equity / MDD
+
+            data[col] = [equity, MDD, equity_to_MDD]
+        df_result = pd.DataFrame(data=data, index=['Equity', 'MDD', 'Equity / MDD'])
+        return df_result.T
