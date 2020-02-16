dbccd49f1123858c56fc212f5667214c41e1e70d
An-Shiang Tsai <ast850328@gmail.com>
2020-02-16 22:25:31 +0800

    Add: Window class

--- Window.py
+++ Window.py
@@ -1,49 +1,53 @@
+import math
+import pandas as pd
 class Window:
 
-    def __init__(self, df, weight_dict, assets):
+    def __init__(self, df_t1, df_t2, selector, model, assets):
+        self.df_t1 = df_t1
+        self.df_t2 = df_t2
+        self.selector = selector
+        self.model = model
         self.assets = assets
-        self.df = df
-        self.weight_dict = weight_dict
-        self.results = []
 
-    def cal_performance(self, df, weight, stock):
+    def _cal_performance(self, df, weight_dict):
+
+        stocks = weight_dict.keys()
         data = {}
-        assets = self.assets * weight
-        buy_price = df[stock].iloc[0]
-        sell_price = df[stock].iloc[-1]
-        shares = math.floor(assets / buy_price)
-        equity_change = sell_price - buy_price
-        equity = equity_change * shares
+        for stock in stocks:
+            df_stock = df.loc[:, [stock]]
 
-        MDD = 0
-        peak = buy_price
-        for index, row in df.iterrows():
-            price = row[stock]
-            if price >= peak:
-                peak = price
-            DD = price - peak
-            if DD <= MDD:
-                MDD = DD
-        MDD = MDD * shares
+            weight = weight_dict[stock]
+            assets = self.assets * weight
+            buy_price = df_stock.iloc[0].values[0]
+            sell_price = df_stock.iloc[-1].values[0]
+            shares = math.floor(assets / buy_price)
+            equity_change = sell_price - buy_price
+            equity = equity_change * shares
+            MDD = 0
+            peak = buy_price
+            for index, row in df_stock.iterrows():
+                price = row[stock]
+                if price >= peak:
+                    peak = price
+                DD = price - peak
+                if DD <= MDD:
+                    MDD = DD
+            MDD = 0 - MDD
+            MDD = MDD * shares
+            equity_to_MDD = equity / MDD
+            data[stock] = [assets, weight, shares, equity_change, equity, MDD, equity_to_MDD]
 
-        data['stock'] = stock
-        data['assets'] = assets
-        data['weight'] = weight
-        data['shares'] = shares
-        data['equity_change'] = equity_change
-        data['equity'] = equity
-        data['MDD'] = MDD
-#         data['Date'] = df.loc[start_t2, 'Date'] + '~' + df.loc[end_t2-1, 'Date']
-        data['Date'] = df['Date'].iloc[-1]
+        df_result = pd.DataFrame(data=data, index=['Assets', 'Weight', 'Shares', 'Equity Change', 'Equity', 'MDD', 'Equity / MDD'])
+        date = df['Date'].iloc[-1]
 
-        return data
+        return df_result.T, date
 
-    def get_results(self):
-        return self.results
-
-    def play_window(self):
-        for stock in self.weight_dict:
-            weight = self.weight_dict[stock]
-            df = self.df_origin.loc[:, ['Date', stock]]
-            data = self.cal_performance(df, weight, stock)
-            self.results.append(data)
+    def play(self):
+        selected_stocks, df_selected = self.selector.select(self.df_t1)
+        df_t1 = self.df_t1.loc[:, selected_stocks]
+        weight_dict = self.model.get_weight(df_t1)
+        selected_stocks.append('Date')
+        cols = selected_stocks
+        df_t2 = self.df_t2.loc[:, cols]
+        df_result, date = self._cal_performance(df_t2, weight_dict)
+        return df_result, date
