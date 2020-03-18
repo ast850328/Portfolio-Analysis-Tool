2815c0bbe161c946a1a0fb8f2c55591c030cc024
An-Shiang Tsai <ast850328@gmail.com>
2020-03-18 15:36:29 +0800

    Add: index, program start point

---
+++ index.py
@@ -0,0 +1,9 @@
+import sys
+from MainWindow import MainWindow
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
